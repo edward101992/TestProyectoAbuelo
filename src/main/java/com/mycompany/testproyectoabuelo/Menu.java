2ded58a158bc69297f1cc0a7a090b5e167d1ff0b
edwardconw <coyoteluxeredward@outlook.es>
2019-03-18 18:41:35 -0500

    Clase Menu Ingresa Personas

--- src/main/java/com/mycompany/testproyectoabuelo/Menu.java
+++ src/main/java/com/mycompany/testproyectoabuelo/Menu.java
@@ -14,28 +14,64 @@ import java.util.Scanner;
  */
 public class Menu {
     
-    public HashMap<Integer, Persona> mapaAbuelo;
-    
+    public HashMap<Integer, Persona> mapaAbuelo;  
     Scanner scanner = new Scanner(System.in);
-    
     int cod;
     String envia;
     
     
-    Public void Menu(){
-    
-            System.out.println("Escriba el Codigo de la Persona");
-            cod = scanner.nextInt();
-            mapaAbuelo = new HashMap<>();
-            ingresarMapa();
-            buscar(mapaAbuelo,cod);
-            
+   public void Menu() {      
+        System.out.println("Digite el codigo que desea buscar:");
+        cod = scanner.nextInt();
+        mapaAbuelo = new HashMap<>();
+        ingresarMapa();
+        buscar(mapaAbuelo,cod);
     }
     
     private void ingresarMapa(){
     
     mapaAbuelo = new HashMap<>();
-    mapaAbuelo.put(1, new Persona(1, "Juan", "Galindo"));
+    mapaAbuelo.put(1, new Persona(1, "Juan"));
     
     }
+    
+    private String buscar(){
+        Persona Abuelo = null;
+        Persona Padre = null;
+        Persona Hijo = null;
+         for(Integer key: mapaAbuelo.keySet()){
+            Abuelo = mapaAbuelo.get(key);
+            if(cod == key){
+                System.out.println("Abuelo: " + Abuelo.getNombre());
+            }
+            for(Integer key2: Abuelo.getDatos().keySet()){
+                Padre = Abuelo.getDatos().get(key2);
+                if(cod == key2){                    
+                    System.out.println("Abuelo: " + Abuelo.getNombre());
+                    System.out.println("            Padre: " + Padre.getNombre());
+                }
+                if(cod == key){
+                    System.out.println("            Padre : " + Padre.getNombre());
+                }
+                for(Integer key3: Padre.getDatos().keySet()){
+                    Hijo = Padre.getDatos().get(key3);
+                    if(cod == key3){      
+                        System.out.println("Abuelo: " + Abuelo.getNombre());
+                        System.out.println("          Padre: " + Padre.getNombre());                        
+                        System.out.println("                     Hijo: " + Hijo.getNombre());
+                        break;
+                    }
+                    if(cod == key2){
+                        System.out.println("                     Hijo: " + Hijo.getNombre());
+                    }
+                    if(cod == key){
+                        System.out.println("                     Hijo: " + Hijo.getNombre());
+                    }
+                }
+            }
+        }
+        return envia;
+    }
+    
 }
+
